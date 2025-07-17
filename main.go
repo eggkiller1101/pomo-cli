084cbe1f2d1d357ec31c469bc8a0535bb0a9ae37
Akiko <akiko@Akikos-MacBook-Air.local>
2025-07-17 23:23:57 +0800

    feature: adjust tick logic to enable ticking at the beginning time

--- main.go
+++ main.go
@@ -12,45 +12,53 @@ type model struct {
 	timeLeft time.Duration
 }
 
-type tickMsg struct{}
+type tickMsg time.Time
 
 func (m model) Init() tea.Cmd {
-	return tea.Tick(time.Minute, func(t time.Time) tea.Msg {
-		return tickMsg{}
+	return tick()
+}
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
 	})
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
 	case tickMsg:
 		if m.timeLeft <= 0 {
 			return m, tea.Quit
 		}
 		m.timeLeft -= time.Second
-		return m, tea.Tick(time.Second, func(t time.Time) tea.Msg {
-			return tickMsg{}
-		})
+		return m, tick()
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		}
 	}
+
 	return m, nil
 }
 
 func (m model) View() string {
-	totalTime := int(m.timeLeft.Seconds())
-	mins := totalTime / 60
-	secs := totalTime % 60
-	return fmt.Sprintf("Time left: %02dm:%02ds\nPress q to quit.", mins, secs)
+	total := int(m.timeLeft.Seconds())
+	if total < 0 {
+		total = 0
+	}
+	mins := total / 60
+	secs := total % 60
+	return fmt.Sprintf("⏳ Time left: %02dm:%02ds\nPress q to quit.\n", mins, secs)
 }
 
 func main() {
 	m := model{timeLeft: 10 * time.Minute}
 	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
-		fmt.Println("Error starting program:", err)
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 }
